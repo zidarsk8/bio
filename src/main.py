c3350ca586299b32713adf2166de8147bd6c2457
Miha Zidar <zidarsk8@gmail.com>
2013-01-10 01:51:47 +0100

    labels and stuff

--- src/main.py
+++ src/main.py
@@ -13,17 +13,15 @@ def getReverseIndex(joined):
 
     return ri
 
-def getData():
+def getData(size = 100000):
     
     l = [i.strip().split("|")[:2] 
-            for i in open("data/morbidmap").readlines()]
+            for i in open("data/morbidmap").readlines()[:size]]
 
     l = map(lambda x: (
         re.search('\W*([A-Za-z -]*[A-Za-z]).*', x[0]).group(1), 
         set([i.strip() for i in x[1].split(",")]) )
         ,l)
-
-    l = l[:2000]
 
     joined = collections.defaultdict(set)
     for (i,j) in l:
@@ -67,33 +65,38 @@ def getClusters(g, iterLimit=100):
 
     return clusters
 
-    
 
-joined = getData()
+def plotG(G, ngenes, iterations = 50):
+
+    nclusters = getClusters(G)
+
+    pos = nx.spring_layout(G, iterations=iterations)
+    nx.draw(G, pos)
+    #pylab.savefig("dsull.pdf")
+
+    pylab.clf() #prepare a new figure
+
+    nodes = G.nodes() #fix node positions
+    nx.draw_networkx_nodes(G, pos, nodes,
+        node_size = [ 50*ngenes[a] for a in nodes],
+        node_color = [ nclusters[a] for a in nodes ],
+        linewidths = 0.1,
+        alpha=0.4) #just because the colors are dark
+
+    # turn this on when cluster
+    #nx.draw_networkx_labels(G, pos)
+
+    nx.draw_networkx_edges(G, pos, alpha=0.2)
+
+    pylab.axis("off")
+    pylab.show()
+    #pylab.savefig("nicer.pdf")
+
+joined = getData(3000)
 
 G = getNxGraph(joined)
-            
-nclusters = getClusters(G)
-ngenes = {name:len(genes) for name, genes in joined.items()}
 
-pos = nx.spring_layout(G, iterations=50)
-nx.draw(G, pos)
-pylab.savefig("dsull.pdf")
+size = lambda x: len(x)**(8/10.)
+ngenes = {name:size(genes) for name, genes in joined.items()}
 
-pylab.clf() #prepare a new figure
-
-nodes = G.nodes() #fix node positions
-nx.draw_networkx_nodes(G, pos, nodes,
-    node_size = [ 50*ngenes[a] for a in nodes],
-    node_color = [ nclusters[a] for a in nodes ],
-    linewidths = 0.1,
-    alpha=0.4) #just because the colors are dark
-
-# turn this on when cluster
-#nx.draw_networkx_labels(G, pos)
-
-nx.draw_networkx_edges(G, pos, alpha=0.2)
-
-pylab.axis("off")
-pylab.show()
-#pylab.savefig("nicer.pdf")
+plotG(nx.connected_component_subgraphs(G)[0], ngenes)
